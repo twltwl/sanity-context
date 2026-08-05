4df3c75d210e99a90fc0bfacca664671a6b181b9
twltwl <tommy@nexure.io>
2026-08-05 09:42:23 +0200

    fix(icon): fixed casing and props

--- src/components/ContextIcon.tsx
+++ src/components/ContextIcon.tsx
@@ -1,14 +1,25 @@
-import {forwardRef} from 'react'
+import {forwardRef, useId} from 'react'
 
 /** @public */
 export const ContextIcon = forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>(
-  function ContextIcon(_props, _ref) {
+  function ContextIcon(props, ref) {
+    // SVG ids are document-global and the gradient is referenced by `url(#...)`, so it has to be
+    // unique per instance. `useId` returns colons, which are legal in an id but trip up selectors.
+    const gradientId = `sanity-context-gradient-${useId().replace(/:/g, '')}`
+
     return (
-      <svg width="18" height="18" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 128 128"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+        ref={ref}
+      >
         <defs>
-          <linearGradient id="g" x1="0" y1="0" x2="1" y2="0">
-            <stop offset="0%" stop-color="#6366F1" />
-            <stop offset="100%" stop-color="#06B6D4" />
+          <linearGradient id={gradientId} x1="0" y1="0" x2="1" y2="0">
+            <stop offset="0%" stopColor="#6366F1" />
+            <stop offset="100%" stopColor="#06B6D4" />
           </linearGradient>
         </defs>
 
@@ -20,7 +31,7 @@ export const ContextIcon = forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElemen
           rx="8"
           fill="none"
           stroke="#6366F1"
-          stroke-width="6"
+          strokeWidth="6"
         />
         <rect
           x="80"
@@ -30,15 +41,15 @@ export const ContextIcon = forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElemen
           rx="8"
           fill="none"
           stroke="#06B6D4"
-          stroke-width="6"
+          strokeWidth="6"
         />
 
         <path
           d="M48 64 C60 40, 68 88, 80 64"
           fill="none"
-          stroke="url(#g)"
-          stroke-width="6"
-          stroke-linecap="round"
+          stroke={`url(#${gradientId})`}
+          strokeWidth="6"
+          strokeLinecap="round"
         />
       </svg>
     )
